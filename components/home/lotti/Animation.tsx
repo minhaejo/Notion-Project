3e1bf3e79bab21a537216e072a0bc11dc8504fc0
minhaejo <robo888@naver.com>
2022-09-20 19:09:28 +0900

    reset modules

--- components/home/lotti/Animation.tsx
+++ components/home/lotti/Animation.tsx
@@ -3,7 +3,7 @@ import Lottie from "react-lottie-player";
 // Alternatively:
 // import Lottie from 'react-lottie-player/dist/LottiePlayerLight'
 
-import lottieJson from "../../public/animation.json";
+import lottieJson from "/public/animation.json";
 
 export default function Animation() {
   return <Lottie loop animationData={lottieJson} play />;
